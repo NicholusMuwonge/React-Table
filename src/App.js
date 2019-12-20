70498042d8649e1a8d25096149de7549a7b1b4b6
NicholusMuwonge <nicholusmuwonge@gmail.com>
2019-12-20 21:17:14 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React,{Fragment} from 'react';
+import './App.css';
+import Content from './PageContent';
+
+const App = () => (
+  <Fragment>
+    <Content />
+  </Fragment>
+);
+
+export default App;
